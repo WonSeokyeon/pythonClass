1a09a1e64c17a90c2c816c026ae9b11420cec463
Won Seokyeon <tjrdus110@gmail.com>
2026-06-26 10:04:20 +0900

    [파이썬] 7장 실습문제

--- Chapter07/exam1.py
+++ Chapter07/exam1.py
@@ -15,3 +15,38 @@ cal_average_gold(12000,15000,18000)
 # cal_kda(10, 2, 7)
 # 실행결과
 # 8.5
+
+# def cal_kda(kill, death, assist):
+#   return (kill +assist) / (death)
+
+# kda = cal_kda(10, 2, 7)
+# print("비율 계산 :",  format(kda))
+
+# Quiz) 표준 체중을 구하는 프로그램을 작성하시오
+# * 표준 체중 : 각 개인의 키에 적당한 체중
+# (성별에 따른 공식) 
+# 남자 : 키(m) x 키(m) x 22 
+# 여자 : 키(m) x 키(m) x 21
+# 조건1 : 표준 체중은 별도의 함수 내에서 계산
+# 함수명 : std_weight
+# 전달값 : 키(height), 성별(gender) 
+# 조건2 : 표준 체중은 소수점 둘째자리까지 표시
+# (출력 예제) 
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
+
+
+
+def std_weight(height , gender):
+  height1 = height/100  #m로 변경해야됨
+  if gender  == "남자" or gender  == "남" :
+    return height1 *height1 *22
+  elif gender  =="여자" or gender == "여":
+    return height1 *height1 *21
+  else:
+    return 0
+
+height =  int (input("키를 입력해주세요(cm)"))
+gender = input("성별을 입력해주세요")
+
+weight =  std_weight(height , gender)  
+print(f"키{height}cm {gender}의 표준체중은 {weight:.2f}kg")
